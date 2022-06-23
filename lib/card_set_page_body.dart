2c7d0c49327f73bb3f1b18bd4cfc1956217a27eb
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2022-06-22 20:30:28 -0500

    I put some parameters in each of the functions that build the card. In this way, when we call the integratedCard method it is going to be mandatory to pass to it the required parameters (number, icon ant texts). The result is a nourished card.

--- lib/card_set_page_body.dart
+++ lib/card_set_page_body.dart
@@ -15,128 +15,170 @@ class CardSetPageBody extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            integratedCard(context),
-            SizedBox(height: 10),
-            integratedCard(context),
-            SizedBox(height: 10),
-            integratedCard(context),
+            integratedCard(
+                stepNumber: 1,
+                stepIcon: Icon(
+                  Icons.hail,
+                  size: 35,
+                  color: Colors.white,
+                ),
+                stepText1: 'Ingresa: ',
+                stepText2: '✓ RFC a 13 posiciones',
+                stepText3: '✓ Correo electrónico personal',
+                stepText4: '✓ Número celular a 10 posiciones'),
+            SizedBox(height: 1),
+            integratedCard(
+              stepNumber: 2,
+              stepIcon:
+                  Icon(Icons.label_important, size: 35, color: Colors.white),
+              stepText1: '',
+              stepText2: 'Ingresa la documentación',
+              stepText3: 'requerida por la autoridad: ',
+              stepText4: '✓ Identificación Oficial',
+            ),
+            SizedBox(height: 1),
+            integratedCard(
+                stepNumber: 3,
+                stepIcon: Icon(Icons.unarchive, size: 35, color: Colors.white),
+                stepText1: '',
+                stepText2: '✓ Confirma tu identidad',
+                stepText3: 'a través de un video',
+                stepText4: '✓ Firma la solicitud'),
           ],
         ),
       ),
     );
   }
-}
 
-Widget integratedCard(context) {
-  return Container(
-    height: 190,
-    //color: Colors.blueAccent,
-    child: Stack(
-      children: [
-        Center(
-          child: Container(
-            height: 120,
-            child: Stack(
-              children: [
-                planetCard(context),
-                planetThumbnail(context),
-              ],
+  Widget integratedCard({
+    required int stepNumber,
+    required Icon stepIcon,
+    required String stepText1,
+    required String stepText2,
+    required String stepText3,
+    required String stepText4,
+  }) {
+    return Container(
+      height: 190,
+      //color: Colors.blueAccent,
+      child: Stack(
+        children: [
+          Center(
+            child: Container(
+              height: 120,
+              child: Stack(
+                children: [
+                  planetCard(
+                      stepText1: stepText1,
+                      stepText2: stepText2,
+                      stepText3: stepText3,
+                      stepText4: stepText4),
+                  planetThumbnail(stepIcon: stepIcon),
+                ],
+              ),
             ),
           ),
-        ),
-        ethereumElement(context),
-      ],
-    ),
-  );
-}
+          ethereumElement(stepNumber: stepNumber),
+        ],
+      ),
+    );
+  }
 
-Widget ethereumElement(context) {
-  return Align(
-    alignment: Alignment.topRight,
-    child: Container(
-      width: 120,
-      height: 50,
-      margin: EdgeInsets.only(top: 10, right: 16),
-      alignment: FractionalOffset.topRight,
-      decoration: BoxDecoration(
-          color: Color(0xff833F4C),
-          border: Border.all(width: 4.0, color: Colors.white),
-          borderRadius: BorderRadius.all(Radius.circular(30.0))),
-      child: Center(
-        child: Text(
-          'PASO N',
-          style: TextStyle(color: Colors.white),
+  Widget ethereumElement({
+    required int stepNumber,
+  }) {
+    return Align(
+      alignment: Alignment.topRight,
+      child: Container(
+        width: 120,
+        height: 50,
+        margin: EdgeInsets.only(top: 10, right: 16),
+        alignment: FractionalOffset.topRight,
+        decoration: BoxDecoration(
+            color: Color(0xff833F4C),
+            border: Border.all(width: 4.0, color: Colors.white),
+            borderRadius: BorderRadius.all(Radius.circular(30.0))),
+        child: Center(
+          child: Text(
+            'PASO ' + stepNumber.toString(),
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 
-Widget planetThumbnail(context) {
-  return Container(
-    margin: EdgeInsets.symmetric(vertical: 16),
-    alignment: FractionalOffset.centerLeft,
-    child: CircleAvatar(
-      backgroundColor: Color(0xffD9BCA3),
-      radius: 50,
+  Widget planetThumbnail({
+    required Icon stepIcon,
+  }) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 16),
+      alignment: FractionalOffset.centerLeft,
       child: CircleAvatar(
-        radius: 36,
-        backgroundColor: Colors.white,
+        backgroundColor: Color(0xffD9BCA3),
+        radius: 50,
         child: CircleAvatar(
+          radius: 36,
+          backgroundColor: Colors.white,
+          child: CircleAvatar(
             radius: 33,
             backgroundColor: Color(0xff833F4C),
-            child: Icon(
-              Icons.document_scanner_rounded,
-              color: Colors.white,
-              size: 34,
-            )),
-      ),
-    ),
-  );
-}
-
-Widget planetCard(context) {
-  return Container(
-    height: 124,
-    width: MediaQuery.of(context).size.width,
-    margin: EdgeInsets.only(left: 46),
-    decoration: BoxDecoration(
-      color: Color(0xffD9BCA3),
-      shape: BoxShape.rectangle,
-      borderRadius: BorderRadius.circular(8.0),
-      boxShadow: <BoxShadow>[
-        BoxShadow(
-          color: Colors.black12,
-          blurRadius: 10.0,
-          offset: Offset(0.0, 10.0),
+            child: stepIcon,
+          ),
         ),
-      ],
-    ),
-    child: Center(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-            child: Text(
-          'Text No 1',
-          style: TextStyle(color: Colors.white),
-        )),
-        Container(
-            child: Text(
-          'Text No 2',
-          style: TextStyle(color: Colors.white),
-        )),
-        Container(
-            child: Text(
-          'Text No 3',
-          style: TextStyle(color: Colors.white),
-        )),
-        Container(
-            child: Text(
-          'Text No 4',
-          style: TextStyle(color: Colors.white),
-        )),
-      ],
-    )),
-  );
+      ),
+    );
+  }
+
+  Widget planetCard({
+    context,
+    required String stepText1,
+    required String stepText2,
+    required String stepText3,
+    required String stepText4,
+  }) {
+    return Container(
+      height: 124,
+      width: 400,
+      margin: EdgeInsets.only(left: 46),
+      decoration: BoxDecoration(
+        color: Color(0xffD9BCA3),
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10.0,
+            offset: Offset(0.0, 10.0),
+          ),
+        ],
+      ),
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+              child: Text(
+            stepText1,
+            style: TextStyle(color: Colors.black, fontSize: 16),
+          )),
+          Container(
+              child: Text(
+            stepText2,
+            style: TextStyle(color: Colors.black, fontSize: 16),
+          )),
+          Container(
+              child: Text(
+            stepText3,
+            style: TextStyle(color: Colors.black, fontSize: 16),
+          )),
+          Container(
+              child: Text(
+            stepText4,
+            style: TextStyle(color: Colors.black, fontSize: 16),
+          )),
+        ],
+      )),
+    );
+  }
 }
