9fd064aebf2805343439a76b08a9f1e1a54590ed
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2022-06-29 23:26:10 -0500

    Adding the buttons section to the identity verification page. Each one with its own icon. Moreover, I've added functionality to the Get Another Quote Button. As I click on it, the quoteSpaceContainer will deploy a new quote (inside it, I placed another scrollable container) from a predetermined list with several quotes using setState.

--- lib/identity_verification.dart
+++ lib/identity_verification.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class IdentityVerification extends StatefulWidget {
@@ -28,7 +30,8 @@ class _IdentityVerificationState extends State<IdentityVerification> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           inspirationalQuoteCard(context),
-          indicationButtons(context),
+          SizedBox(height: 20),
+          recordAndAnotherQuoteButtons(context),
         ],
       ),
     );
@@ -36,7 +39,7 @@ class _IdentityVerificationState extends State<IdentityVerification> {
 
   inspirationalQuoteCard(context) {
     return Container(
-        height: 450,
+        height: 420,
         //color: Color(0xff833F4C),
         width: MediaQuery.of(context).size.width,
         margin: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
@@ -74,9 +77,30 @@ class _IdentityVerificationState extends State<IdentityVerification> {
   }
 
   Widget quoteSpaceContainer(context) {
+    List<String> listOfQuotes = [
+      'Sobre la montaña florida sueltan los caballos en el cielo otoñal',
+      'Yo soñaba cada día poder alcanzar la playa',
+      'El misterio de la vida no es un problema a resolver, sino una realidad a experimentar',
+      'Estar solo no tiene nada que ver con cuantas personas hay alrededor',
+      'El hombre débil se vuelve fuerte cuando no tiene nada, porque sólo entonces puede sentir la locura de la desesperación',
+      'Mientras el corazón late, mientras el cuerpo y alma siguen juntos, no puedo admitir que cualquier criatura dotada de voluntad tiene necesidad de perder la esperanza en la vida'
+          'Cada libro, cada volumen que ves aquí, tiene un alma. El alma de la persona que lo escribió y de aquellos que lo leyeron, vivieron y soñaron con él. Cada vez que un libro cambia de manos, cada vez que alguien baja sus ojos a las páginas, su espíritu crece y se fortalece',
+      'Mi consejo es: nunca hagas mañana lo que puedes hacer hoy. La procrastinación es la ladrona del tiempo',
+      'Conseguir lo que quieres es tan difícil como no conseguir lo que quieres. Porque entonces tienes que averiguar qué hacer con ello, en lugar de averiguar qué hacer sin ello.',
+      'Crearía un perfume que no sólo fuera humano, sino sobrehumano. Un aroma de ángel, tan indescriptiblemente bueno y pletórico de vigor que quien lo oliera quedaría hechizado y no tendría más remedio que amar a la persona que lo llevara, o sea, amarle a él, Grenouille, con todo su corazón'
+          'Nada hay en el mundo, ni hombre ni diablo ni cosa alguna, que sea para mí tan sospechoso como el amor, pues éste penetra en el alma más que cualquier otra cosa. Nada hay que ocupe y ate más al corazón que el amor. Por eso, cuando no dispone de armas para gobernarse, el alma se hunde, por el amor, en la más honda de las ruinas'
+          'Amor y deseo son dos cosas diferentes; que no todo lo que se ama se desea, ni todo lo que se desea se ama',
+      'Labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Tengo esperanza o podría no vivir',
+      'Luchar hasta el último aliento',
+      'Sea un hombre o sea más que un hombre. Sea firme con su propósito y firme como una piedra ',
+    ];
+    final random = Random();
+    final quoteChoosen = listOfQuotes[random.nextInt(listOfQuotes.length)];
+
     return Container(
       //color: Colors.blue[200],
-      height: 300,
+      height: 220,
       width: MediaQuery.of(context).size.width,
       margin: EdgeInsets.only(left: 20, right: 20, top: 190),
       alignment: FractionalOffset.bottomCenter,
@@ -102,9 +126,9 @@ class _IdentityVerificationState extends State<IdentityVerification> {
                 child: Container(
                   margin: EdgeInsets.only(right: 18),
                   child: Text(
-                    'Labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    quoteChoosen,
                     style: TextStyle(fontSize: 24),
-                    textAlign: TextAlign.justify,
+                    textAlign: TextAlign.center,
                   ),
                 ),
               ),
@@ -144,11 +168,88 @@ class _IdentityVerificationState extends State<IdentityVerification> {
     );
   }
 
-  indicationButtons(context) {
+  recordAndAnotherQuoteButtons(context) {
+    List<String> listOfQuotes = [
+      'Sobre la montaña florida sueltan los caballos en el cielo otoñal',
+      'Yo soñaba cada día poder alcanzar la playa',
+      'El misterio de la vida no es un problema a resolver, sino una realidad a experimentar',
+      'Estar solo no tiene nada que ver con cuantas personas hay alrededor',
+      'El hombre débil se vuelve fuerte cuando no tiene nada, porque sólo entonces puede sentir la locura de la desesperación',
+      'Mientras el corazón late, mientras el cuerpo y alma siguen juntos, no puedo admitir que cualquier criatura dotada de voluntad tiene necesidad de perder la esperanza en la vida'
+          'Cada libro, cada volumen que ves aquí, tiene un alma. El alma de la persona que lo escribió y de aquellos que lo leyeron, vivieron y soñaron con él. Cada vez que un libro cambia de manos, cada vez que alguien baja sus ojos a las páginas, su espíritu crece y se fortalece',
+      'Mi consejo es: nunca hagas mañana lo que puedes hacer hoy. La procrastinación es la ladrona del tiempo',
+      'Conseguir lo que quieres es tan difícil como no conseguir lo que quieres. Porque entonces tienes que averiguar qué hacer con ello, en lugar de averiguar qué hacer sin ello.',
+      'Crearía un perfume que no sólo fuera humano, sino sobrehumano. Un aroma de ángel, tan indescriptiblemente bueno y pletórico de vigor que quien lo oliera quedaría hechizado y no tendría más remedio que amar a la persona que lo llevara, o sea, amarle a él, Grenouille, con todo su corazón'
+          'Nada hay en el mundo, ni hombre ni diablo ni cosa alguna, que sea para mí tan sospechoso como el amor, pues éste penetra en el alma más que cualquier otra cosa. Nada hay que ocupe y ate más al corazón que el amor. Por eso, cuando no dispone de armas para gobernarse, el alma se hunde, por el amor, en la más honda de las ruinas'
+          'Amor y deseo son dos cosas diferentes; que no todo lo que se ama se desea, ni todo lo que se desea se ama',
+      'Labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Tengo esperanza o podría no vivir',
+      'Luchar hasta el último aliento',
+      'Sea un hombre o sea más que un hombre. Sea firme con su propósito y firme como una piedra ',
+    ];
+    final random = Random();
+    final quoteChoosen = listOfQuotes[random.nextInt(listOfQuotes.length)];
     return Container(
-      margin: EdgeInsets.only(bottom: 10),
-      color: Colors.green,
-      height: 140,
+      //color: Colors.green,
+      height: 180,
+      child: Column(
+        children: [
+          buildSingleButton(
+            stepsTitle: 'GRABAR',
+            stepsIcon: Icons.videocam_outlined,
+            stepsOnClicked: () {},
+            stepsColor: Color(0xff833F4C),
+            stepsAvatarColor: Color(0xff833F4C),
+          ),
+          SizedBox(height: 25),
+          buildSingleButton(
+            stepsTitle: 'OBTENER OTRA FRASE',
+            stepsIcon: Icons.format_quote,
+            stepsOnClicked: () => setState(() {
+              quoteChoosen;
+            }),
+            stepsColor: Color(0xff833F4C),
+            stepsAvatarColor: Color(0xff833F4C),
+          ),
+        ],
+      ),
     );
   }
+
+  Widget buildSingleButton({
+    required String stepsTitle,
+    required IconData stepsIcon,
+    required VoidCallback stepsOnClicked,
+    required Color stepsColor,
+    required Color stepsAvatarColor,
+  }) =>
+      Container(
+        height: 55,
+        margin: EdgeInsets.symmetric(horizontal: 40),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              primary: stepsColor,
+              onPrimary: Colors.white,
+              textStyle: TextStyle(fontSize: 20)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                stepsTitle,
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(width: 10),
+              CircleAvatar(
+                radius: 23,
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                    radius: 21,
+                    backgroundColor: stepsAvatarColor,
+                    child: Icon(stepsIcon, size: 24, color: Colors.white)),
+              ),
+            ],
+          ),
+          onPressed: stepsOnClicked,
+        ),
+      );
 }
